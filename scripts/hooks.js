db8424466e494b9487ff65fbf3a4f65cbb6b97b3
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2021-01-24 08:28:25 -0500

    Bypass Foundry Macro System

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -2,7 +2,6 @@ import { logger } from './logger.js';
 import * as settings from './settings.js';
 
 const pack_key = "world-scripter.world-scripter-macros";
-const wait = async (ms) => new Promise((resolve)=> setTimeout(resolve, ms));
 
 Hooks.on('init', ()=>{
   logger.info("Register All Settings");
@@ -10,16 +9,19 @@ Hooks.on('init', ()=>{
 });
 
 Hooks.on('ready', async ()=>{
-  logger.debug("Macro Data (INIT) | ", game.packs.get(pack_key));
+  logger.debug("Macro Data (INIT) | ", pack_key);
   
   let pack = game.packs.get(pack_key);
   let contents = await pack.getContent();
 
-  for(let content of contents)
-  {
-    content.execute();
-    await wait(100); //might not be necessary
-  }
+  contents.forEach(content=>{
+    try{
+      eval(content.data.command);
+    }catch(err){
+      logger.error(`Failed to execute : `, content.data.name);
+    }
+    
+  });
 });
 
 
--- scripts/logger.js
+++ scripts/logger.js
@@ -7,4 +7,9 @@ export class logger {
       if (game.settings.get('world-scripter', 'debug'))
           logger.info("DEBUG | ", ...args);
   }
+
+  static error(...args){
+    logger.info("ERROR | ", ...args);
+    ui.notifications.error(`Error `, ...args);
+  }
 }
