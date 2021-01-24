c2fca29fde7addb0a52140380f2132ad91deea23
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2021-01-24 17:51:32 -0500

    Fixed Tabbing

--- scripts/logger.js
+++ scripts/logger.js
@@ -1,11 +1,11 @@
 export class logger {
   static info(...args) {
-      console.log("World Scripter | ", ...args);
+    console.log("World Scripter | ", ...args);
   }
 
   static debug(...args) {
-      if (game.settings.get('world-scripter', 'debug'))
-          logger.info("DEBUG | ", ...args);
+    if (game.settings.get('world-scripter', 'debug'))
+      logger.info("DEBUG | ", ...args);
   }
 
   static error(...args){
