e2849116dcbf88dc578b4b6d20b7a5217a007f3d
Matthew Monahan <64717385+Kekilla0@users.noreply.github.com>
2021-11-30 17:42:39 -0500

    Update
    Fixed pack locking issue.
    Updated for 9.X compatibility.

--- scripts/scripter.js
+++ scripts/scripter.js
@@ -6,7 +6,7 @@ export class scripter{
     logger.debug("Accessing Pack | ", settings.KEY);
     let pack = game.packs.get(settings.KEY);
     if(!pack) return logger.error(`${settings.TITLE} ${settings.i18n("error.scriptMissing")}`);
-    let contents = await pack.getContent();
+    let contents = await pack.getDocuments();
     contents.forEach(macro => scripter.execute_macro(macro));
   }
 
@@ -32,14 +32,22 @@ export class scripter{
 
   static async add_To_Compendium(_id){
     logger.info("Context Clicked | ", _id);
+
     let macro = game.macros.get(_id);
     if(!macro) return logger.error(`${settings.i18n("error.macroID")}, ${_id}`);
+
     let pack = game.packs.get(settings.KEY);
     if(!pack) return logger.error(`${settings.TITLE} ${settings.i18n("error.scriptMissing")}`);
+
+    let status = pack.locked;
+    if(!status) pack.configure({ locked : !status });
+
     let index = await pack.getIndex();
     if(index.find(ele => ele.name === macro.name))
       await pack.updateEntity({_id : index.find(ele => ele.name === macro.name)._id, command : macro.data.command });
     else 
       await pack.createEntity(macro.data);
+
+    if(!status) pack.configure({ locked : status });
   }
 }
